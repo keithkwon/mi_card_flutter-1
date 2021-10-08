9f2aa03362a15b46b680a08613cb87302e3fc135
Keith Kwon <keithkwon.dev@gmail.com>
2021-10-08 13:19:36 +0900

    ADD widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,26 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp()
   );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width : 100.0,
+            margin:EdgeInsets.fromLTRB(10, 20, 30, 40),
+            color:Colors.white,
+            child: Text('Hello')
+          ),
+        ),
+      ),
+    );
+  }
 }
